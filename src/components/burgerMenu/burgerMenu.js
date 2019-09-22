a201323088af00f64b6ca30c6fef430574ddae0d
MartinLindblad <martin.lindblad1@gmail.com>
2019-09-22 23:57:50 +0200

    Can't see list items

--- src/components/burgerMenu/burgerMenu.js
+++ src/components/burgerMenu/burgerMenu.js
@@ -1,33 +1,238 @@
 import React from "react"
 
-const BurgerMenu = () => (
-  <div class="burger">
-    <div class="hamburger">
-      <div class="line4"></div>
-      <div class="line3"></div>
-      <div class="line2"></div>
-      <div class="line1"></div>
-    </div>
-    <nav>
-      <ul class="main">
-        <li>
-          <a href="#">Home</a>
-        </li>
+class BurgerMenu extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      menuOpen: true,
+    }
+  }
+  handleMenuClick() {
+    this.setState({ menuOpen: !this.state.menuOpen })
+  }
+  handleLinkClick() {
+    this.setState({ menuOpen: false })
+  }
+  render() {
+    // ;<div class="burger">
+    //   <div class="hamburger">
 
-        <li>
-          <a href="index.html#me-pic">About</a>
-        </li>
+    //   </div>
+    // </div>
+    const styles = {
+      burger: {
+        position: "fixed",
+        width: "80px",
+        height: "60px",
+        top: "20px",
+        right: "20px",
+        zIndex: "10",
+        display: "flex",
+      },
+    }
+    const menu = ['About Us','Our Products','Services','FAQ','Contact Us']
+    const menuItems = menu.map((val, index) => {
+      return (
+        <MenuItem
+          key={index}
+          delay={`${index * 0.1}s`}
+          onClick={() => {
+            this.handleLinkClick()
+          }}
+        >
+          {val}
+        </MenuItem>
+      )
+    })
+    return (
+      console.log(menuItems),
+      (
+        <div style={styles.burger}>
+          <MenuButton
+            open={this.state.menuOpen}
+            onClick={() => this.handleLinkClick()} // Burger Lines
+          ></MenuButton>
+          <Menu open={this.state.menuOpen}>{menuItems}</Menu>
+         
+        </div>
+      )
+    )
+  }
+}
 
-        <li>
-          <a href="index.html#work">Works</a>
-        </li>
+class MenuItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hover: false,
+    }
+  }
 
-        <li>
-          <a href="index.html#contact-h2">Contact</a>
-        </li>
-      </ul>
-    </nav>
-  </div>
-)
+  handleHover() {
+    this.setState({ hover: !this.state.hover })
+  }
+
+  render() {
+    const styles = {
+      navLi: {
+        // color: '#ffffff',
+        fontFamily: "Raleway",
+        textDecoration: "none",
+        textTransform: "uppercase",
+        fontSize: "1.5rem",
+        display: "block",
+        letterSpacing: "5px",
+        fontWeight: "600",
+        padding: "10px 0",
+        transition: "all 0.3s ease",
+        color: this.state.hover ? "#b7ac7f" : "white",
+      },
+    }
+    return (
+      console.log(this.props.children),
+      <li
+        style={styles.menuLi}
+        onMouseEnter={() => {
+          this.handleHover()
+        }}
+        onMouseLeave={() => {
+          this.handleHover()
+        }}
+        onClick={this.props.onClick}
+      >
+        {this.props.children}
+      </li>
+    )
+  }
+}
+
+class Menu extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: this.props.open ? this.props.open : false,
+    }
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.open !== this.state.open) {
+      this.setState({ open: nextProps.open })
+    }
+  }
+  render() {
+    const styles = {
+      nav: {
+        backgroundColor: "#2a2a2ad0",
+        position: "fixed",
+        zIndex: "9",
+        top: "0",
+        height: "100%",
+        maxWidth: "515px",
+        width: "100%",
+        padding: "100px 40px 60px 40px",
+        overflowY: "auto",
+        transition: "transform 0.55s cubic-bezier(0.785, 0.135, 0.15, 0.86)",
+        transform: "translateX(0%)",
+      },
+      ul: {
+        listStyleType: "none",
+      },
+    }
+    return (
+      <nav style={styles.nav}>
+        {this.state.open ? (
+          <ul style={styles.ul}>
+            
+          </ul>
+        ) : (
+          null
+        )}
+      </nav>
+    )
+  }
+}
+
+class MenuButton extends React.Component {
+  constructor(props) {
+    super(props)
+    {
+      // this.state = {
+      //   open: this.props.open ? this.props.open : false,
+      //   color: this.props.open ? this.props.open : "#b7ac7f",
+      // }
+    }
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.open !== this.state.open) {
+      this.setState({ open: nextProps.open })
+    }
+  }
+
+  handleClick() {
+    this.setState({ open: !this.state.open })
+  }
+
+  render() {
+    const styles = {
+      hamburger: {
+        display: "flex",
+        alignSelf: "center",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        width: "100px",
+        height: "40px",
+        cursor: "pointer",
+      },
+      line: {
+        alignSelf: "flex-end",
+        height: "4px",
+        width: "100%",
+        background: "#3E4651",
+      },
+      line1: {
+        backgroundColor: "#b7ac7f",
+        width: "100%",
+        animation: "burger-hover 3s infinite",
+      },
+      line2: {
+        backgroundColor: "#b7ac7f",
+        width: "35%",
+        transition: "all 600ms ease-in-out",
+        animation:
+          "burger-hover 3s infinite ease-in-out alternate forwards 200ms",
+      },
+      line3: {
+        backgroundColor: "#b7ac7f",
+        width: "50%",
+        transition: "all 600ms ease-in-out",
+        animation:
+          "burger-hover 3s infinite ease-in-out alternate forwards 400ms",
+      },
+      line4: {
+        backgroundColor: "#b7ac7f",
+        width: "75%",
+        transition: "all 600ms ease-in-out",
+        animation:
+          "burger-hover 3s infinite ease-in-out alternate forwards 600ms",
+      },
+    }
+    return (
+      <div
+        style={styles.hamburger}
+        onClick={
+          this.props.onClick
+            ? this.props.onClick
+            : () => {
+                this.handleClick()
+              }
+        }
+      >
+        <div style={{ ...styles.line, ...styles.line4 }}></div>
+        <div style={{ ...styles.line, ...styles.line3 }}></div>
+        <div style={{ ...styles.line, ...styles.line2 }}></div>
+        <div style={{ ...styles.line, ...styles.line1 }}></div>
+      </div>
+    )
+  }
+}
 
 export default BurgerMenu
